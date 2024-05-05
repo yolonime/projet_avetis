095ed260f46667f3c004d74fee290aa07f2e1ae2
yolonime <ti.boug.95100@gmail.com>
2024-05-05 23:15:28 +0200

    2nd commit

--- documentation.md
+++ documentation.md
@@ -3,4 +3,17 @@ npm run dev
 
 explication du code:
 
-home page: 
+home page: app/page
+create page: app/addPlace/page
+edit page: app/editPlace/page
+methode CRUD: app/api
+
+connexion à la base de données: libs/mongodb.js
+création de la base de données(shema): models/place.js
+
+problem rencontré:
+j'ai commencé le projet en reprenant votre code de base et ai travailler dessus en utilisant la fonctionnalité des code spaces de github. j'ai donc travailler sur le projet en ligne et je me suis rendu compte que ne pourais pas recupere le travail produit.
+j ai donc recommence de zero en suivant un tuto sur youtube...
+que j ai essaye de reproduire et d'adapter et voici le resultat :(
+
+
--- models/place.js
+++ models/place.js
@@ -1,15 +1,34 @@
 import mongoose, { Schema } from "mongoose";
 
-const placeSchema = new Schema(
-  {
-    title: String,
-    description: String,
-  },
-  {
-    timestamps: true,
-  }
-);
 
-const Place = mongoose.models.Placec || mongoose.model("Place", placeSchema);
+  const placeSchema = new Schema(
+    {
+      name: String,
+      description: String,
+      type: String,
+      address: String,
+      city: String,
+      zipCode: String,
+      country: String,
+      stars: String,
+      price: String,
+      cuisine: String,
+      artisticCurrent: String,
+      artType: String,
+      freeOrPaid: String,
+      priceMuseum: String,
+      typeBar: String,
+      priceBar: String,
+      typePark: String,
+      publicOrPrivate: String,
+      freeOrPaidPark: String,
+      pricePark: String,
+    },
+    {
+      timestamps: true,
+    }
+  );
 
-export default Place;
+  const Place = mongoose.models.Place || mongoose.model("Place", placeSchema);
+
+  export default Place;
